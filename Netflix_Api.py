2553154eb2f7950190dc941a7d598e92e92d95c5
VicAnto99 <A01339529@itesm.mx>
2020-05-25 18:23:57 -0500

    Mejora de la interfaz

--- Netflix_Api.py
+++ Netflix_Api.py
@@ -7,6 +7,14 @@ from tkinter import Label, StringVar, Button, Entry, Tk, Frame, messagebox
 window = Tk()
 window.title("Netflix")
 window.maxsize(1010, 610)
-window.config(bg = "skyblue")
+window.config(bg = "black")
+
+#Left_Frame_design and Right_Frame_design and center
+center_frame_1 = Frame(window, width = 990, height = 590, bg='red')
+center_frame_1.grid(row = 0, column = 0, padx = 10, pady = 10)
+left_frame = Frame(center_frame_1, width = 290, height = 570, bg='black')
+left_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+right_frame = Frame(center_frame_1, width = 650, height = 570, bg='black')
+right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
 
 window.mainloop()
