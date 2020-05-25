d289e31214c3d181ae9437c08241b98772daaaf0
VicAnto99 <A01339529@itesm.mx>
2020-05-25 17:47:55 -0500

    Ventana creada

---
+++ Netflix_Api.py
@@ -0,0 +1,12 @@
+#Imports
+from tkinter import Label, StringVar, Button, Entry, Tk, Frame, messagebox
+
+#Definitions
+
+#Main
+window = Tk()
+window.title("Netflix")
+window.maxsize(1010, 610)
+window.config(bg = "skyblue")
+
+window.mainloop()
