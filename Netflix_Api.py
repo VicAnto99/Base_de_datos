90074019e90f45a44ecb89cd3ac0f99cc24e4b7b
VicAnto99 <A01339529@itesm.mx>
2020-05-25 18:39:24 -0500

    Logo de Netflix

--- Netflix_Api.py
+++ Netflix_Api.py
@@ -1,5 +1,6 @@
 #Imports
 from tkinter import Label, StringVar, Button, Entry, Tk, Frame, messagebox
+from PIL import Image, ImageTk
 
 #Definitions
 
@@ -16,5 +17,12 @@ left_frame = Frame(center_frame_1, width = 290, height = 570, bg='black')
 left_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
 right_frame = Frame(center_frame_1, width = 650, height = 570, bg='black')
 right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+#Left_Frame_design
+load = Image.open("Netflix.png")
+render = ImageTk.PhotoImage(load)
+img = Label(left_frame, image = render, bg = "black").grid(row = 0, column = 0, padx = 0, pady = 0)
+
+#Right_Frame_design
 
 window.mainloop()
