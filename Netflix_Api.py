8545692bd541564ea4d0fee363ade54d90c15c7d
VicAnto99 <A01339529@itesm.mx>
2020-06-01 20:34:12 -0500

    Interfaz con mongo

--- Netflix_Api.py
+++ Netflix_Api.py
@@ -1,18 +1,69 @@
 #Imports
-from tkinter import Label, StringVar, Button, Entry, Tk, Frame, messagebox
+from tkinter import Label, StringVar, Button, Entry, Tk, Frame, messagebox, Menu
 from PIL import Image, ImageTk
 from bson.objectid import ObjectId
 from pymongo import MongoClient
+from functools import partial
 import redis
 
 def Store_cache(db, cache, cache_key, reset=True, limit=1000):
-    #if reset:
-        #cache.delete(cache_key)
-    #Query_data(db,cache,cache_key)
-    #Query_statistics(db,cache,cache_key)
-    #Insert_in_database(db,cache,cache_key)
-    #Delete_of_database(db,cache,cache_key)
-    Update_in_database(db,cache,cache_key)
+    if reset:
+       cache.delete(cache_key)
+    #Window
+    window = Tk()
+    window.title("Netflix")
+    window.iconbitmap('N.ico')
+    window.maxsize(1010, 610)
+    window.config(bg = "black")
+    menubar = Menu(window, bg = 'gray17', fg = 'gray63')
+    window.config(menu = menubar)
+
+    #Variables
+    right_display = StringVar()
+    right_display.set('NETFLIX')
+
+    #Left_Frame_design and Right_Frame_design and center
+    center_frame_1 = Frame(window, width = 990, height = 590, bg='red')
+    center_frame_1.grid(row = 0, column = 0, padx = 10, pady = 10)
+    left_frame = Frame(center_frame_1, width = 290, height = 570, bg='black')
+    left_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
+    right_frame = Frame(center_frame_1, width = 650, height = 570, bg='gray10')
+    right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
+
+    #Menu
+    search = Menu(menubar, tearoff = 0, bg = 'gray17', fg = 'gray63')
+    search.add_command(label = "ID", command = partial(id_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Type", command = partial(type_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Title", command = partial(title_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Director", command = partial(director_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Cast", command = partial(cast_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Country", command = partial(country_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Realease year", command = partial(realease_year_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Rating", command = partial(rating_s,right_display,right_frame,db,cache,cache_key))
+    search.add_command(label = "Search and have the statistics", command = partial(search_s_S,right_display,right_frame,db,cache,cache_key))
+    menubar.add_cascade(label = "Search", menu = search)
+
+    modify = Menu(menubar, tearoff = 0, bg = 'gray17', fg = 'gray63')
+    modify.add_command(label = "Edit", command = partial(edit_m,right_display,right_frame,db,cache,cache_key))
+    modify.add_command(label = "Add", command = partial(add_m,right_display,right_frame,db,cache,cache_key))
+    modify.add_command(label = "Delete", command = partial(delete_m,right_display,right_frame,db,cache,cache_key))
+    menubar.add_cascade(label = "Modify Database", menu = modify)
+
+    #Left_Frame_design
+    load = Image.open("Netflix.png")
+    render = ImageTk.PhotoImage(load)
+    img = Label(left_frame, image = render, bg = "black").grid(row = 0, column = 0, padx = 0, pady = 0)
+    in_1 = Label(left_frame, text = "INSTRUCTIONS", bg = "black", fg = "white", font = ("Verdana", 24)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    in_2 = Label(left_frame, text = "Select the type of your search", bg = "black", fg = "white", font = ("Verdana", 12)).grid(row = 2, column = 0, padx = 5, pady = 5)
+    in_3 = Label(left_frame, text = "or if you prefer", bg = "black", fg = "white", font = ("Verdana", 12)).grid(row = 3, column = 0, padx = 5, pady = 5)
+    in_4 = Label(left_frame, text = "to add, edit or delete someting \n on the", bg = "black", fg = "white", font = ("Verdana", 12)).grid(row = 4, column = 0, padx = 5, pady = 5)
+    in_3 = Label(left_frame, text = "DATABASE", bg = "black", fg = "white", font = ("Verdana", 16)).grid(row = 5, column = 0, padx = 5, pady = 5)
+
+    #Right_Frame_design
+    an_1 = Label(right_frame, text = "Welcome to the app", bg = "gray10", fg = "gray55", font = ("Verdana", 24)).grid(row = 0, column = 0, padx = 5, pady = 5)
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+    window.mainloop()
 
 def Query_data(db, cache, cache_key):
     columns=["show_id","type","title","director","cast","country","date_added","release_year","rating","duration","listed_in","description"]
@@ -66,8 +117,60 @@ def Update_in_database(db, cache, cache_key):
     print("Updated from the database :")
     print(" ",new_query)
 
-if __name__ == '__main__':
+def id_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by ID")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    Query_data(db,cache,cache_key)
 
+def type_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by TYPE")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def title_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by TITLE")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def director_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by DIRECTOR")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def cast_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by CAST")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def country_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by COUNTRY")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def realease_year_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by REALEASE YEAR")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def rating_s(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by RATING")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+
+def search_s_S(right_display, right_frame, db,cache,cache_key):
+    right_display.set("Search by STATISTIC")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    Query_statistics(db,cache,cache_key)
+
+def edit_m(right_display, right_frame, db,cache,cache_key):
+    right_display.set("EDIT something to the database")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    Update_in_database(db,cache,cache_key)
+
+def add_m(right_display, right_frame, db,cache,cache_key):
+    right_display.set("ADD something to the database")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    Insert_in_database(db,cache,cache_key)
+
+def delete_m(right_display, right_frame, db,cache,cache_key):
+    right_display.set("DELETE something to the database")
+    an_2 = Label(right_frame, textvariable = right_display, bg = "gray10", fg = "gray55", font = ("Verdana", 18)).grid(row = 1, column = 0, padx = 5, pady = 5)
+    Delete_of_database(db,cache,cache_key)
+
+if __name__ == '__main__':
     #Connections
     client = MongoClient()
     db = client.Netflix
@@ -75,34 +178,4 @@ if __name__ == '__main__':
     cache = redis.from_url('redis://localhost:6379', db=0)
     cache_key = "cache_set"
     MAX_EXPIRE_DURATION = 24 * 3600
-    #Busqueda y almacenamiento de queries
     Store_cache(col, cache, cache_key)
-    #
-
-    #Definitions
-
-    #Main
-    """window = Tk()
-    window.title("Netflix")
-    window.maxsize(1010, 610)
-    window.config(bg = "black")
-
-    #Left_Frame_design and Right_Frame_design and center
-    center_frame_1 = Frame(window, width = 990, height = 590, bg='red')
-    center_frame_1.grid(row = 0, column = 0, padx = 10, pady = 10)
-    left_frame = Frame(center_frame_1, width = 290, height = 570, bg='black')
-    left_frame.grid(row = 0, column = 0, padx = 10, pady = 10)
-    right_frame = Frame(center_frame_1, width = 650, height = 570, bg='black')
-    right_frame.grid(row = 0, column = 1, padx = 10, pady = 10)
-
-    #Left_Frame_design
-    load = Image.open("Netflix.png")
-    render = ImageTk.PhotoImage(load)
-    img = Label(left_frame, image = render, bg = "black").grid(row = 0, column = 0, padx = 0, pady = 0)
-
-    #Right_Frame_design
-
-
-    
-
-window.mainloop()"""
